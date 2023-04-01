3a4ae8f74b40d7dcce5b7e7b5f04c014a5f999a6
noname <48761044+noname0310@users.noreply.github.com>
2023-04-01 17:34:11 +0900

    update css

--- client/src/game/scripts/UIBuilder.tsx
+++ client/src/game/scripts/UIBuilder.tsx
@@ -4,10 +4,25 @@ import * as TWE from "the-world-engine";
 import css from "./UiBuilder.module.css";
 
 export class UIBuilder extends TWE.Component {
-    public start(): void {
-        const rootElement = this.engine.domElement;
+    private _renderRoot: HTMLDivElement | null = null;
 
-        Nano.render(<this.createUI />, rootElement, false);
+    public start(): void {
+        this.render();
+    }
+
+    public onDestroy(): void {
+        if (this._renderRoot !== null) this._renderRoot.remove();
+    }
+
+    public render(): void {
+        if (this._renderRoot === null) {
+            const rootElement = this.engine.domElement;
+            const renderRootElement = document.createElement("div");
+            renderRootElement.className = css.renderRoot;
+            rootElement.appendChild(renderRootElement);
+            this._renderRoot = renderRootElement;
+        }
+        Nano.render(<this.createUI />, this._renderRoot);
     }
 
     private createUI(): any {
@@ -16,14 +31,16 @@ export class UIBuilder extends TWE.Component {
                 <button class={css.closeButton}>X</button>
                 <div>
                     <label>
-                        <input type="radio" name="test" value="1" />
-                        Test
+                        <input type="radio" name="tilemap front" value="1" />
+                        tilemap front
                     </label>
-                </div>
-                <div>
                     <label>
-                        <input type="radio" name="test" value="2" />
-                        Test
+                        <input type="radio" name="tilemap back" value="2" />
+                        tilemap back
+                    </label>
+                    <label>
+                        <input type="radio" name="collider" value="3" />
+                        collider
                     </label>
                 </div>
             </div>
